0bee588fddd083fe269282a6d9e815bd82b88773
Namita Prakash <69874499+namita-prakash@users.noreply.github.com>
2021-02-09 13:56:53 -0800

    Naprakas/prep for rx kql update (#21)
    * Moved custom scalar functions over to realtimekql
    
    * rolled net462 forward to net472

---
+++ Source/RealTimeKql/CustomRxKqlFunctions/GetProcessName.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Runtime.Caching;
+using System.Reactive.Kql;
+using System;
+
+namespace RealTimeKql
+{
+    public static partial class CustomScalarFunctions
+    {
+        [KqlScalarFunction("getprocessname")]
+        public static string GetProcessName(uint pid)
+        {
+            if (processNameCache.Contains(pid.ToString()))
+            {
+                return processNameCache.Get(pid.ToString()).ToString();
+            }
+
+            string ret = string.Empty;
+            Process proc = null;
+            try { proc = Process.GetProcessById((int)pid); }
+            catch (Exception) { }
+
+            try
+            {
+                if (proc != null)
+                {
+                    ret = proc.ProcessName;
+                }
+            }
+            catch
+            {
+            }
+            finally
+            {
+                if (string.IsNullOrWhiteSpace(ret))
+                {
+                    ret = pid.ToString();
+                }
+            }
+
+            // Only keep the item in cache only for 10 seconds to avoid issues with process ID reuse.  
+            processNameCache.Add(pid.ToString(), ret, DateTime.Now.AddSeconds(10));
+            return ret;
+        }
+
+        private static MemoryCache processNameCache = new MemoryCache("ProcessNameCache");
+    }
+}
