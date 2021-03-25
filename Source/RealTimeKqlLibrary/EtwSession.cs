c35834cfd6b193cd838945b7100cacbf3f7fab72
REDMOND\naprakas <naprakas@microsoft.com>
2021-03-25 13:08:54 -0700

    added modifier subject to etw session

--- Source/RealTimeKqlLibrary/EtwSession.cs
+++ Source/RealTimeKqlLibrary/EtwSession.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kusto.Cloud.Platform.Security;
+using System;
+using System.Collections.Generic;
 using System.Security.Principal;
 
 namespace RealTimeKqlLibrary
@@ -6,9 +8,19 @@ namespace RealTimeKqlLibrary
     public class EtwSession: EventComponent
     {
         private readonly string _sessionName;
+        private readonly bool _kqlProcessing;
         public EtwSession(string sessionName, IOutput output, params string[] queries) : base(output, queries)
         {
             _sessionName = sessionName;
+
+            if(queries == null || queries.Length == 0 || string.IsNullOrEmpty(queries[0]))
+            {
+                _kqlProcessing = false;
+            }
+            else
+            {
+                _kqlProcessing = true;
+            }
         }
 
         override public bool Start()
@@ -21,12 +33,27 @@ namespace RealTimeKqlLibrary
                 return false;
             }
 
-            // Check if etw session specified exists
-            // TODO
-
             // Getting Etw event stream from Tx.Windows
             var eventStream = Tx.Windows.EtwTdhObservable.FromSession(_sessionName);
-            return Start(eventStream, "etw" + _sessionName, true);
+
+            ModifierSubject<IDictionary<string, object>> modifier = null;
+            if(!_kqlProcessing)
+            {
+                // Events go to output as Tx.Windows.EtwTdhEvent, 
+                // which implements lazy deserialization
+                // Need to force total deserialization before events hit output
+                // to avoid termination errors about accessing protected memory
+
+                modifier = new ModifierSubject<IDictionary<string, object>>(OutputAction);
+            }
+
+            return Start(eventStream, "etw" + _sessionName, true, modifier);
+        }
+
+        private IDictionary<string, object> OutputAction(IDictionary<string, object> obj)
+        {
+            obj.GetEnumerator();
+            return obj;
         }
     }
 }
