a9f0bce442a0f6f9a110c9800f501b2b9315cf5c
REDMOND\naprakas <naprakas@microsoft.com>
2022-01-18 13:03:49 -0800

    Changed way of getting kqlpython project dir in setup.py

--- Source/KqlPython/setup.py
+++ Source/KqlPython/setup.py
@@ -2,10 +2,12 @@ import os
 import glob
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'VERSION.txt'), encoding='utf-8') as f:
+import kqlpythondir
+
+with open(os.path.join(kqlpythondir.directory, 'VERSION.txt'), encoding='utf-8') as f:
 	version = f.read()
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(kqlpythondir.directory, 'README.md'), encoding='utf-8') as f:
 	long_description = f.read()
 
 setup(
